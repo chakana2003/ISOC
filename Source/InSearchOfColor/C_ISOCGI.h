1ac6abea55ab98e5a421bb1af88666b96bf58ed4
KangNengs <chakana2003@gmail.com>
2019-09-03 01:07:01 +0900

    C++ 프로젝트로 변경.

---
+++ Source/InSearchOfColor/C_ISOCGI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "C_ISOCGI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INSEARCHOFCOLOR_API UC_ISOCGI : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/InSearchOfColor/InSearchOfColor.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "InSearchOfColor.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, InSearchOfColor, "InSearchOfColor" );
